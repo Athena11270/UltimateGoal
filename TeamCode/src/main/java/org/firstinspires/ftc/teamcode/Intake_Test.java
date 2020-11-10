622cd21dfaac032ffeae529de292d8b0014de753
Athena <athena11270@gmail.com>
2020-11-09 19:56:39 -0600

    we now have an input tester to find broken controllers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake_Test.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Intake Test", group="Linear Opmode")
@@ -10,8 +11,9 @@ public class Intake_Test extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor LR = null;
-    private DcMotor LL = null;
+    //private DcMotor LR = null;
+    //private DcMotor LL = null;
+    private DcMotor OT = null;
 
     @Override
     public void runOpMode() {
@@ -21,14 +23,16 @@ public class Intake_Test extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        LR = hardwareMap.get(DcMotor.class, "LR");
-        LL = hardwareMap.get(DcMotor.class, "LL");
+        //LR = hardwareMap.get(DcMotor.class, "LR");
+        //LL = hardwareMap.get(DcMotor.class, "LL");
+        OT = hardwareMap.get(DcMotor.class, "OT");
         //yoo are ___
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        LR.setDirection(DcMotor.Direction.FORWARD);
-        LL.setDirection(DcMotor.Direction.REVERSE);
-
+        //LR.setDirection(DcMotor.Direction.FORWARD);
+        //LL.setDirection(DcMotor.Direction.REVERSE);
+        OT.setDirection(DcMotorSimple.Direction.FORWARD);
+        OT.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
@@ -38,12 +42,15 @@ public class Intake_Test extends LinearOpMode {
 
             double IPower = gamepad1.left_stick_y;
 
-            LL.setPower(IPower);
-            LR.setPower(IPower);
+            //LL.setPower(IPower);
+            //LR.setPower(IPower);
+
+            OT.setPower(gamepad1.right_stick_y);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "IL (%.2f), IR (%.2f)", LL.getPower(), LR.getPower());
+            //telemetry.addData("Motors", "IL (%.2f), IR (%.2f), OT (%.2f),", LL.getPower(), LR.getPower(), OT.getPower());
+            telemetry.addData("Motor", "OT (%.2f",OT.getPower());
             telemetry.update();
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inputtest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@TeleOp(name="input test", group="Linear Opmode")
+public class inputtest extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            telemetry.addData("A", gamepad1.a + "; B: " + gamepad1.b + "; X: " + gamepad1.x + "; Y: " + gamepad1.y);
+            telemetry.addData("Up", gamepad1.dpad_up + "; Left: " + gamepad1.dpad_left + "; Right: " + gamepad1.dpad_right + "; Down: " + gamepad1.dpad_down);
+            telemetry.addData("AS", "LS[" + gamepad1.left_stick_x + ", " + gamepad1.left_stick_y + "]; RS[" + gamepad1.right_stick_x + ", " + gamepad1.right_stick_y + "]");
+            telemetry.addData("BP", "L: " + gamepad1.left_bumper + "; R: " + gamepad1.right_bumper);
+            telemetry.addData("TR", "L: " + gamepad1.left_trigger + "; R: " + gamepad1.right_trigger);
+            telemetry.update();
+        }
+    }
+}
